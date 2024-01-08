aab1157ca365a1dea8d7452548531378f9886a9a
Trần Nguyễn Tuấn Anh <87246397+gumbbb@users.noreply.github.com>
2024-01-08 12:18:52 +0700

    Update main.py

--- Lab8/BaiTap2/main.py
+++ Lab8/BaiTap2/main.py
@@ -15,23 +15,23 @@ file_path = os.path.join(script_directory, 'lrc_xray.pkl')
 
 if os.path.exists(file_path):
     with open(file_path, 'rb') as input:
-        # Rest of your code here
+        
+        model = pkl.load(input)
+        
+        st.header('Upload an image')
+        image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']))
+        
+        if image is not None:
+          image = Image.open(image)
+          st.image(image, caption='Test image')
+        
+          if st.button('Predict'):
+            image = image.resize((227*227*3, 1))
+            vector = np.array(image)
+            label = str((model.predict(vector))[0])
+        
+            st.header('Result')
+            st.text(class_list[label])
 else:
     print(f"File not found: {file_path}")
 
-model = pkl.load(input)
-
-st.header('Upload an image')
-image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']))
-
-if image is not None:
-  image = Image.open(image)
-  st.image(image, caption='Test image')
-
-  if st.button('Predict'):
-    image = image.resize((227*227*3, 1))
-    vector = np.array(image)
-    label = str((model.predict(vector))[0])
-
-    st.header('Result')
-    st.text(class_list[label])
